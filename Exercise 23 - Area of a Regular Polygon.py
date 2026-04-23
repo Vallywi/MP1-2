6691568cc9302682891bb1310e56bde96c021d2b
Valiwri <jecynvallirieturbanos@gmail.com>
2026-04-23 04:02:20 -1200

    Just updated the files

--- Exercise 23 - Area of a Regular Polygon.py
+++ Exercise 23 - Area of a Regular Polygon.py
@@ -1,1 +1,13 @@
 # Exercise 23 - Area of a Regular Polygon
+
+import math
+
+side_length = float(input("Enter the length of the side of the equilateral triangle: "))
+side_number = int(input("Enter the number of sides of the polygon: "))
+
+def calculate_area(side_length, side_number):
+    area = (side_number * (float(side_length) ** 2)) / (4 * math.tan(math.pi / float(side_number)))
+    return area
+a = calculate_area(side_length, side_number)
+
+print(f"The area of the polygon is: {a} ")  
