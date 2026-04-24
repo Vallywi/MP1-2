0974d6c46d030ecd7f18455e1c512f4a570d1a30
Valiwri <jecynvallirieturbanos@gmail.com>
2026-04-23 21:57:12 -1200

    added exer 24 & exer 25

--- Exercise 25 - Units of Time (Again).py
+++ Exercise 25 - Units of Time (Again).py
@@ -1,1 +1,10 @@
 # Exercise 25 - Units of Time (Again)
+
+number_seconds = int(input("Enter the number of seconds: "))
+days = number_seconds // (24 * 3600)
+number_seconds = number_seconds % (24 * 3600)
+hours = number_seconds // 3600
+number_seconds = number_seconds % 3600
+minutes = number_seconds // 60
+number_seconds = number_seconds % 60
+print(f"{days} days, {hours} hours, {minutes} minutes, and {number_seconds} seconds")
